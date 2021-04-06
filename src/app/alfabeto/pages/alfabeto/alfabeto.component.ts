709ff1ac30d69812a196bc45c577d62fa414231b
John Rubio <johnrubio27@gmail.com>
2021-04-06 09:16:06 -0500

    Implemetacion de operaciones de alfabeto Visualizacion de las operaciones.

--- src/app/alfabeto/pages/alfabeto/alfabeto.component.ts
+++ src/app/alfabeto/pages/alfabeto/alfabeto.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
+import { Alfabetos } from '../../models/alfabeto.model';
 
 @Component({
   selector: 'app-alfabeto',
@@ -7,9 +9,55 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AlfabetoComponent implements OnInit {
 
-  constructor() { }
+  alfabetoArrayA!: string[];
+  alfabetoArrayB!: string[];
+
+  estado = false;
+
+  // Operaciones de alfabeto y lenguaje
+  unionAlfabeto!: string[];
+
+  diferenciaAB!: string[];
+  diferenciaBA!: string[];
+
+  interseccion!: string[];
+
+  // Formularios Reactivos
+  myForm!: FormGroup;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.myForm = this.fb.group(
+      {
+        arrayA: ['', [Validators.required]],
+        arrayB: ['', [Validators.required]]
+      }
+    );
   }
 
+
+  // Funcion utilizada para obtener los dos alfabetos ingresado por el usuario
+  capturarArrayAlfabetos(): void {
+    const { arrayA, arrayB, potencia } = this.myForm.value;
+    this.alfabetoArrayA = limpiarRepetidos((arrayA.trim()).split(','));
+    this.alfabetoArrayB = limpiarRepetidos((arrayB.trim()).split(','));
+    this.operacionesValidas(this.alfabetoArrayA , this.alfabetoArrayB, potencia);
+  }
+
+  operacionesValidas(lenguaje1: string[], lenguaje2: string[], potencia: number): void {
+    this.estado = true;
+    const objAlfabeto = new Alfabetos(lenguaje1, lenguaje2);
+    this.unionAlfabeto = objAlfabeto.unionAlfabeto();
+    this.diferenciaAB = objAlfabeto.diferenciaAlfabetoAB();
+    this.diferenciaBA = objAlfabeto.diferenciaAlfabetoBA();
+    this.interseccion = objAlfabeto.intersecion();
+  }
+
+}
+
+function limpiarRepetidos(myArr: string[]): string[] {
+  return myArr.filter((el, index) => myArr.indexOf(el) === index);
 }
