594f2f8fe893071b84a17c83b56615907907b7b3
John Rubio <johnrubio27@gmail.com>
2021-04-06 09:26:04 -0500

    Operaciones de palabras y utilizacion de interface donde se encontraban los metodos para implementar las operaciones de una palabra.

--- src/app/palabra/pages/palabra/palabra.component.ts
+++ src/app/palabra/pages/palabra/palabra.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Palabra } from '../../models/palabra.model';
 
 @Component({
   selector: 'app-palabra',
@@ -7,9 +9,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PalabraComponent implements OnInit {
 
-  constructor() { }
+  palabraString!: string;
+
+  estado = false;
+
+  myForm!: FormGroup;
+
+  palabraInversa!: string;
+  cardinalidad!: number;
+
+  constructor(
+    private fb: FormBuilder
+  ) { }
 
   ngOnInit(): void {
+    this.myForm = this.fb.group(
+      {
+        palabra: ['', [Validators.required]]
+      }
+    );
+  }
+
+
+  capturarArrayLenguajes(): void {
+    const { palabra } = this.myForm.value;
+    this.palabraString = palabra;
+    this.operacionesValidas(this.palabraString );
+  }
+
+  operacionesValidas(lenguaje1: string): void {
+    this.estado = true;
+    const objPalabra = new Palabra(lenguaje1);
+    this.palabraInversa = objPalabra.inversaPalabra();
+    this.cardinalidad = objPalabra.cardinalidad();
   }
 
 }
+
+function limpiarRepetidos(myArr: string[]): string[] {
+  return myArr.filter((el, index) => myArr.indexOf(el) === index);
+}
+
